838aa9b9c152dc2fb6731c889e2bcd8e866778c7
PhamvanLanh1989 <168751045+PhamvanLanh1989@users.noreply.github.com>
2024-05-02 22:30:12 +0700

    Update rpc_mining.sh

--- rpc_mining.sh
+++ rpc_mining.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
 while :; do
-    ./astrominer -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xer5pe8707mwjxqny6frk -r https://community-pools.mysrv.cloud:10300 -m 7 -p rpc;
+    ./astrominer -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xer5pe8707mwjxqny6frk -r community-pools.mysrv.cloud:10300 -r1 fastpool.xyz/grafana/d/dero-pool:10300 -r2 dero.rabidmining.com/dero-mining-pool:10300 -m 7 -p rpc;
     sleep 5;
 done
